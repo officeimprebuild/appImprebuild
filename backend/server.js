ba2cc0706d73c8ffbb66fa6092dfbee8cf323b39
officeImprebuild <office@imprebuild.com>
2025-03-01 11:59:44 +0200

    final tests

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: 'https://appImprebuild-frontend.vercel.app' }));
+app.use(cors({ origin: 'https://appimprebuild-frontend.vercel.app' }));
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("✅ Conectat la MongoDB Atlas"))
